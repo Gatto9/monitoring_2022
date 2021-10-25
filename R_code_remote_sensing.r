ec7baa8ff3c29ba477b3f1b5bf3ec35739646e8d
Gatto9 <92572838+Gatto9@users.noreply.github.com>
2021-10-25 11:03:19 +0200

    Create R_code_remote_sensing.r

---
+++ R_code_remote_sensing.r
@@ -0,0 +1,8 @@
+# R code for ecosystem monitoring by remote sensing
+# First of all, we need to install additional packages
+# raster package to manage image data
+# https://
+
+install.packages("raster")
+
+library(raster)
