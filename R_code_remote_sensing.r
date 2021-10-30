124397cb466e66a4f934f60d8648455fe7ce8d42
Gatto9 <92572838+Gatto9@users.noreply.github.com>
2021-10-30 20:31:30 +0200

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -12,4 +12,22 @@ library(raster)
 # object cannot be number
 l2011 <- brick("p224r63_2011.grd")
 
+l2011
+
+plot(l2011)
+
+#B1 us the reflectance in the blue band
+#B2 us the reflectance in the green band
+#B3 us the reflectance in the red band
+
+
+colorRampPalette(c("black","grey","light grey"))(100) 
+
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100) 
+plot(l2011, col=cl)
+
+
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+
 
