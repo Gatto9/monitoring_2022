bcafa3067b6bc36033c6966ed7df957c2b16958a
Gatto9 <92572838+Gatto9@users.noreply.github.com>
2021-11-08 10:57:35 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -11,8 +11,11 @@ setwd("C:/lab/")
 
 # we are going to import satellite data
 # object cannot be number
+
 l2011 <- brick("p224r63_2011.grd")
 
+#brick function to import a satellite immage
+#rasterbrick is a multilayer function
 l2011
 
 plot(l2011)
@@ -49,7 +52,7 @@ plot(l2011$B2_sre, col=cl)
 clg <- colorRampPalette (c("dark green","green","light grey"))(100)
 plot(l2011$B2_sre, col=clg)
 
-#do the same thing usig dark blue, blue and light blue, e.g clb
+# do the same thing usig dark blue, blue and light blue, e.g clb
 
 clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
 plot(l2011$B1_sre, col=clb)
@@ -66,5 +69,50 @@ plot(l2011$B2_sre, col=clg)
 
 dev.off() #it close a plotting device (the graphs and so on)
 
+# our eyes cannot see over 0.59 micrometers
+# melenconia è un quadro con il quadrato dei pixels
+# parakana is the most forest with pollutant
+# grd(grid) is a structure that we use to made a code
+# brick function to import a satellite immage
+
+#----------------DAY 3
+
+plot(l2011$B1_sre)
+
+# plot the blue band using a blue colorRampPalette
+clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+
+# multiframe
+
+par(mfrow=c(1,2)) # the first numer is the number of row in the multiframe,
+clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+
+clg <- colorRampPalette (c("dark green","green","light grey"))(100)
+plot(l2011$B1_sre, col=clg)
+
+#confronta i quattro colori
+par(mfrow=c(2,2)) 
 
 
+
+clb <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+
+clr <- colorRampPalette(c("dark red", "red", "pink"))(100)
+plot(l2011$B2_sre, col=clr)
+
+clg <- colorRampPalette(c("dark green", "green", "light green"))(100)
+plot(l2011$B3_sre, col=clg)
+
+clnir <- colorRampPalette(c("red","orange", "yellow"))(100)
+plot(l2011$B4_sre, col=clnir)
+
+dev.off()
+
+par(mfrow=c(2,2))
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")
