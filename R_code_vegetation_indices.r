e6b83fbd354264dcd643ac785256965eba6cc049
Gatto9 <92572838+Gatto9@users.noreply.github.com>
2021-11-16 20:08:01 +0100

    Create R_code_vegetation_indices.r

---
+++ R_code_vegetation_indices.r
@@ -0,0 +1,25 @@
+# two main processes in dynamic ecosystem and chemical cycling
+# first theromadynamics law: energy is conserved, and is lost from organisms as heat
+# second law of thermodynamics: in a ecosystem, every exchange of energy increases the entropy of the universe. energy conversions are not completely efficient
+
+# autotrophs build organic molecules themselves using photosynthesis or chemosynthesis as an energy source
+# Heterotrophs depend on the biosynthetic utput of other organisms.
+# autotrofi (produttori primari) erbivori (consumatori primari) carnivori(consumatori secondari) carnivori che mangiano altri carnivori (consumatori terziari)
+# decomposers are consumers that derive their energy from detritus, non loving organic matter (prokariotes and fungi)
+# decomposition connects all trophic levels
+# primary production in an ecosystem is the amount of ight energy converted to chemical energy by utotrophs during a given time period.
+# The extent of hotosynthetic production sets the spending limit or an ecosystem’s energy budget.
+# The amount of solar radiation reaching the Earth’s urface limits photosynthetic output of ecosystems
+
+# GPP--> GROSS PRIMARY PRODUCTION is the total carbon fixation by terrestrial ecosystems through vegetation photosynthesis
+# NPP--> NET PRIMARY PRODUCTION is GPP minus energy used by primary producers for respiration NPP = GPP - RESPIRATION
+# Standing crop is the total biomass of hotosynthetic autotrophs at a given time.
+
+# Tropical rainforest, estuaries and coral reefs are among the most productive ecosystem per unit area.
+##for this reason what we can do is mantaining a big monitoring of tropical forest since tropical forests are creating most of energy and biomass and chemical cycling
+# Marine ecosystems are relatively unproductive per unit area but contribute for NPP cause of their volume
+
+#how much of this biomass is been lost in time.
+# how the energy is passing from an organisms to another
+# biogeochemical cycles 
+
