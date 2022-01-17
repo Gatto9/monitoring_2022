beea34e4f84502c8fad489dd3d175e824681ff70
Gatto9 <92572838+Gatto9@users.noreply.github.com>
2022-01-17 01:22:53 +0100

    Update R_code_greenland.r

--- R_code_greenland.r
+++ R_code_greenland.r
@@ -4,15 +4,36 @@
 # Time series analysis
 # Greenland increase of temperature
 # Data and code from Emanuela Cosma
+library(raster)
+library(ggplot2)
+library(RStoolbox)
+library(patchwork)
+library(viridis)
+
+setwd("/Users/macdisimonegatto/Desktop/lab/greenland")
 
 
-setwd("/Users/macdisimonegatto/Deskop/lab/greenland")
+# imoport all the file in a difficult way (handwritten)
+lst_2000 <- raster("lst_2000.tif")
+lst_2005 <- raster("lst_2005.tif")
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
+# par and plot all the files, lazy stuff
+par(mfrow= c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+# or i can do the job in a simple way, creating a list of files:
 
 rlist <- list.files(pattern = "lst")
 rlist
-
-
 ## [1] "lst_2000.tif" "lst_2005.tif" "lst_2010.tif" "lst_2015.tif"
+
+
+# with lapply function i give a function to the list that i have choosen and name the object
 import <- lapply(rlist,raster)
 import
 
@@ -25,8 +46,7 @@ import
 ## source : lst_2000.tif
 ## names : lst_2000
 ## values : 0, 65535 (min, max)
-##
-##
+
 ## [[2]]
 ## class : RasterLayer
 ## dimensions : 1913, 2315, 4428595 (nrow, ncol, ncell)
@@ -34,7 +54,7 @@ import
 ## extent : -267676.7, 3313324, -1483987, 1475229 (xmin, xmax, ymin, ymax)
 ## crs : +proj=stere +lat_0=90 +lon_0=-33 +k=0.994 +x_0=2000000 +y_0=2000000 +datum=WGS84 +units=m +no_defs
 ## source : lst_2005.tif
-1
+
 ## names : lst_2005
 ## values : 0, 65535 (min, max)
 ##
@@ -59,6 +79,7 @@ import
 ## source : lst_2015.tif
 ## names : lst_2015
 ## values : 0, 65535 (min, max)
+## this VALUES means that are sixty bit images
 
 TGr <- stack(import)
 TGr
@@ -72,10 +93,14 @@ TGr
 ## min values : 0, 0, 0, 0
 ## max values : 65535, 65535, 65535, 65535
 
+cl <- colorRampPalette(c("blue", "light blue" , "pink" , "yellow"))(100)
 plot(TGr)
 
-
-
+# ggplot of first and final images 2000 vs 2015
+ggplot() + geom_raster(TGr$lst_2000, mapping = aes(x=x, y=y, fill=lst_2000)) + scale_fill_viridis(option="magma")+ ggtitle("LST in 2000)
+ggplot() + geom_raster(TGr$lst_2005, mapping = aes(x=x, y=y, fill=lst_2005)) + scale_fill_viridis(option="magma")+ ggtitle("LST in 2005)
+ggplot() + geom_raster(TGr$lst_2010, mapping = aes(x=x, y=y, fill=lst_2010)) + scale_fill_viridis(option="magma")+ ggtitle("LST in 2010)
+ggplot() + geom_raster(TGr$lst_2015, mapping = aes(x=x, y=y, fill=lst_2015)) + scale_fill_viridis(option="magma")+ ggtitle("LST in 2015)
 
 
 
