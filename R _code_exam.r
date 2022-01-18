c880139d204f46372e5b7b432df7744bff9f005e
Gatto9 <92572838+Gatto9@users.noreply.github.com>
2022-01-18 11:56:58 +0100

    Update R _code_exam.r

--- R _code_exam.r
+++ R _code_exam.r
@@ -13,14 +13,23 @@ library(viridis)
 
 setwd("/Users/macdisimonegatto/Desktop/lab/project")
 
+# i can import the file like that
 fire <- raster("c_gls_BA300_202008010000_GLOBE_S3_V3.0.1.nc")
 
 cl <- colorRampPalette(c('red','yellow','green','black'))(100) 
 
+# i can create a list of the file with the BA pattern 
+rlist <- list.files(pattern="BA") 
+rlist
+
+
+# and apply the raster function in a simple way
+list_rast <-lapply(rlist, raster) # it works if you put in the argument name first and funciont the 2nd
+list_rast
 
 # crop the file and take the south italy images
 ext <- c(15.5, 18, 37.7, 40)
-fire_cropped <- crop(fire, ext)
+fire_cropped <- crop(list_rast, ext)
 plot(fire_cropped)
 plot(fire_cropped, col=cl)
 
