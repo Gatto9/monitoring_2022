75c96782634ba7971200a5df7d91e9d2331455f5
Gatto9 <92572838+Gatto9@users.noreply.github.com>
2022-01-25 02:01:45 +0100

    Update R _code_exam.r

--- R _code_exam.r
+++ R _code_exam.r
@@ -31,9 +31,9 @@ setwd("/Users/macdisimonegatto/Desktop/lab/project")
 
 # i'm curious to see the cover fire in calabrian region during the 2021 summer
 fire <- raster("c_gls_BA300_202008010000_GLOBE_S3_V3.0.1.nc")
-
+raster("c_gls_BA300_202108100000_GLOBE_S3_V1.2.1.nc")
 # Or importing all the files creating a list
-firelist <- list.files(pattern="BA")
+firelist <- list.files(pattern="BA300")
 firelist
 
 cl <- colorRampPalette(c('red','green','yellow'))(100) 
@@ -45,7 +45,10 @@ fire_cropped <- crop(fire, ext)
 plot(fire_cropped)
 plot(fire_cropped, col=cl)
 
-###########
+
+
+
+########################################
 #NDVI 
 library(patchwork)
 library(raster)
@@ -56,14 +59,30 @@ library(viridis)
 library(ncdf4)
 setwd("/Users/macdisimonegatto/Desktop/lab/project")
 
-rlist <- list.files(pattern="NDVI")
+# i can import all the file by hand, so lazy...
+NDVI_2014 <- raster("c_gls_NDVI300_2014_08_010000_GLOBE_PROBAV_V1.0.1.nc")
+# this images are important to consider the impact of the covid on the beavhiour of the popualation and ecomafie during the pandemic era
+NDVI_07_21 <- raster("c_gls_NDVI300_2020_07_210000_GLOBE_PROBAV_V1.0.1.nc")
+NDVI_08_01 <- raster("c_gls_NDVI300_2020_08_010000_GLOBE_PROBAV_V1.0.1.nc")
+NVDI_08_11 <- raster("c_gls_NDVI300_2020_08_110000_GLOBE_PROBAV_V1.0.1.nc")
+NVDI_08_11 <- raster("c_gls_NDVI300_202108010000_GLOBE_OLCI_V2.0.1.nc")
+NVDI_08_11 <- raster("c_gls_NDVI300_202108110000_GLOBE_OLCI_V2.0.1.nc")
+NVDI_08_11 <- raster("c_gls_NDVI300_202108210000_GLOBE_OLCI_V2.0.1.nc")
+
+# and assigning a function to each images
+# or better i can create a list from a common name
+rlist <- list.files(pattern="NDVI300")
 rlist
 # [1] "c_gls_NDVI300_202007210000_GLOBE_PROBAV_V1.0.1.nc" 
 # [2] "c_gls_NDVI300_202008010000_GLOBE_PROBAV_V1.0.1.nc"
 # [3] "c_gls_NDVI300_202008110000_GLOBE_PROBAV_V1.0.1.nc"
+# [5] "c_gls_NDVI300_202108010000_GLOBE_OLCI_V2.0.1.nc"    
+# [6] "c_gls_NDVI300_202108110000_GLOBE_OLCI_V2.0.1.nc"    
+# [7] "c_gls_NDVI300_202108210000_GLOBE_OLCI_V2.0.1.nc"  
 
+# applying a function to every image
 list_rast <- lapply(rlist, raster)
-# list_rast
+list_rast
 
 # [[1]]
 # class      : RasterLayer 
@@ -71,9 +90,10 @@ list_rast <- lapply(rlist, raster)
 # resolution : 0.00297619, 0.00297619  (x, y)
 # extent     : -180.0015, 179.9985, -59.99851, 80.00149  (xmin, xmax, ymin, ymax)
 # crs        : +proj=longlat +datum=WGS84 +no_defs 
-# source     : c_gls_NDVI300_202007210000_GLOBE_PROBAV_V1.0.1.nc 
+# source     : c_gls_NDVI300_2014_08_010000_GLOBE_PROBAV_V1.0.1.nc 
 # names      : Normalized.Difference.Vegetation.Index.333M 
 # zvar       : NDVI 
+
 
 # [[2]]
 # class      : RasterLayer 
@@ -81,9 +101,10 @@ list_rast <- lapply(rlist, raster)
 # resolution : 0.00297619, 0.00297619  (x, y)
 # extent     : -180.0015, 179.9985, -59.99851, 80.00149  (xmin, xmax, ymin, ymax)
 # crs        : +proj=longlat +datum=WGS84 +no_defs 
-# source     : c_gls_NDVI300_202008010000_GLOBE_PROBAV_V1.0.1.nc 
+# source     : c_gls_NDVI300_2020_07_210000_GLOBE_PROBAV_V1.0.1.nc 
 # names      : Normalized.Difference.Vegetation.Index.333M 
-# zvar       : NDVI  
+# zvar       : NDVI 
+
 
 # [[3]]
 # class      : RasterLayer 
@@ -91,26 +112,161 @@ list_rast <- lapply(rlist, raster)
 # resolution : 0.00297619, 0.00297619  (x, y)
 # extent     : -180.0015, 179.9985, -59.99851, 80.00149  (xmin, xmax, ymin, ymax)
 # crs        : +proj=longlat +datum=WGS84 +no_defs 
-# source     : c_gls_NDVI300_202008110000_GLOBE_PROBAV_V1.0.1.nc 
+# source     : c_gls_NDVI300_2020_08_010000_GLOBE_PROBAV_V1.0.1.nc 
 # names      : Normalized.Difference.Vegetation.Index.333M 
 # zvar       : NDVI 
 
-vegetation <- stack(list_rast)
-# vegetation
 
-#class      : RasterStack 
-#dimensions : 47040, 120960, 5689958400, 3  (nrow, ncol, ncell, nlayers)
+# [[4]]
+# class      : RasterLayer 
+# dimensions : 47040, 120960, 5689958400  (nrow, ncol, ncell)
+# resolution : 0.00297619, 0.00297619  (x, y)
+# extent     : -180.0015, 179.9985, -59.99851, 80.00149  (xmin, xmax, ymin, ymax)
+# crs        : +proj=longlat +datum=WGS84 +no_defs 
+# source     : c_gls_NDVI300_2020_08_110000_GLOBE_PROBAV_V1.0.1.nc 
+# names      : Normalized.Difference.Vegetation.Index.333M 
+# zvar       : NDVI 
+
+# [[5]]
+# class      : RasterLayer 
+# dimensions : 47040, 120960, 5689958400  (nrow, ncol, ncell)
+# resolution : 0.00297619, 0.00297619  (x, y)
+# extent     : -180.0015, 179.9985, -59.99851, 80.00149  (xmin, xmax, ymin, ymax)
+# crs        : +proj=longlat +datum=WGS84 +no_defs 
+# source     : c_gls_NDVI300_202108010000_GLOBE_OLCI_V2.0.1.nc 
+# names      : Normalized.Difference.Vegetation.Index.333m 
+# z-value    : 2021-08-01 
+# zvar       : NDVI 
+
+
+# [[6]]
+# class      : RasterLayer 
+# dimensions : 47040, 120960, 5689958400  (nrow, ncol, ncell)
+# resolution : 0.00297619, 0.00297619  (x, y)
+# extent     : -180.0015, 179.9985, -59.99851, 80.00149  (xmin, xmax, ymin, ymax)
+# crs        : +proj=longlat +datum=WGS84 +no_defs 
+# source     : c_gls_NDVI300_202108110000_GLOBE_OLCI_V2.0.1.nc 
+# names      : Normalized.Difference.Vegetation.Index.333m 
+# z-value    : 2021-08-11 
+# zvar       : NDVI 
+
+
+#[[7]]
+#class      : RasterLayer 
+#dimensions : 47040, 120960, 5689958400  (nrow, ncol, ncell)
 #resolution : 0.00297619, 0.00297619  (x, y)
 #extent     : -180.0015, 179.9985, -59.99851, 80.00149  (xmin, xmax, ymin, ymax)
 #crs        : +proj=longlat +datum=WGS84 +no_defs 
-#names      : Normalized.Difference.Vegetation.Index.333M.1, Normalized.Difference.Vegetation.Index.333M.2, Normalized.Difference.Vegetation.Index.333M.3 
+#source     : c_gls_NDVI300_202108210000_GLOBE_OLCI_V2.0.1.nc 
+#names      : Normalized.Difference.Vegetation.Index.333m 
+#z-value    : 2021-08-21 
+# zvar       : NDVI 
 
 
-a <- vegetation$Normalized.Difference.Vegetation.Index.333M.1
-b <- vegetation$Normalized.Difference.Vegetation.Index.333M.2
-c <- vegetation$Normalized.Difference.Vegetation.Index.333M.3 
+##  creation of a stack of this different rasterlayer 
+vegetation <- stack(list_rast)
+vegetation
 
-ext <- c(15.5, 18, 37.7, 40)
+# class      : RasterStack 
+# dimensions : 47040, 120960, 5689958400, 4  (nrow, ncol, ncell, nlayers)
+# resolution : 0.00297619, 0.00297619  (x, y)
+# extent     : -180.0015, 179.9985, -59.99851, 80.00149  (xmin, xmax, ymin, ymax)
+# crs        : +proj=longlat +datum=WGS84 +no_defs 
+# names      : Normalized.Difference.Vegetation.Index.333M.1, 
+#              Normalized.Difference.Vegetation.Index.333M.2, 
+#              Normalized.Difference.Vegetation.Index.333M.3, 
+#              Normalized.Difference.Vegetation.Index.333M.4, 
+#              Normalized.Difference.Vegetation.Index.333m.1,
+#              Normalized.Difference.Vegetation.Index.333m.2,
+#              Normalized.Difference.Vegetation.Index.333m.3
+#create the extenction of coordiantes first (x,y)
+# I decide to crop first because the image have a huge amount of pixels
+
+names(vegetation)<- c("August_2014",  "July_21", "August_01",  "August_11", "August_fire_01", "August_fire_11", "August_fire_21")
+
+ext <- c(15.6, 16.4036, 37.8463, 38.4) # provincia di Reggio, Asprumunti.
+veg_cropped <- crop(vegetation, ext)
+veg_cropped
+
+# class      : RasterBrick 
+# dimensions : 186, 270, 50220, 7  (nrow, ncol, ncell, nlayers)
+# resolution : 0.00297619, 0.00297619  (x, y)
+# extent     : 15.5997, 16.40327, 37.84673, 38.4003  (xmin, xmax, ymin, ymax)
+# crs        : +proj=longlat +datum=WGS84 +no_defs 
+# source     : r_tmp_2022-01-23_235322_7161_69049.grd 
+# names      : August_2014,   July_21, August_01, August_11, August_fire_01, August_fire_11, August_fire_21 
+# min values :      -0.076,    -0.080,    -0.080,    -0.080,         -0.080,         -0.080,         -0.080 
+# max values :   0.9360001, 0.9360001, 0.9360001, 0.9360001,      0.9360000,      0.9360000,      0.9360000
+
+
+plot(veg_cropped)
+plot(veg_cropped$August_2014)
+# i'd obtain a plotting point to see the loss of vegetation in seven years
+plot(veg_cropped$August_2014, veg_cropped$August_fire_21)
+abline(0,1)
+abline(0,1, col="red")
+
+# assign a name for a multitemporal frame
+
+Aug_08_2014 <- veg_cropped[[1]]
+Aug_01_2020 <- veg_cropped[[3]]
+Aug_21_2021 <- veg_cropped[[7]]
+Aug_01_2021 <- veg_cropped[[5]]
+#or
+Aug_08_2014 <- veg_cropped$August_2014
+Aug_01_2020 <- veg_cropped$August_01
+Aug_21_2021 <- veg_cropped$August_fire_21
+Aug_01_2021 <- veg_cropped$August_fire_01
+
+# zoom to a better vision of the point of interest
+extReggio <- c(15.7456, 16.1973, 37.9463, 38.2839) # provincia di Reggio, Asprumunti.
+
+temporal_stack <-stack(Aug_08_2014, Aug_01_2020, Aug_21_2021, Aug_01_2021)
+
+# the situation is dramatically compromised, we must do something
+# the people are out of their minds
+
+plot(Aug_08_2014)
+plot(Aug_01_2021)
+plot(Aug_21_2021)
+
+#create a palette first
+clb<- colorRampPalette(c("darkblue","green","lightblue"))(100)
+clg<- colorRampPalette(c("darkgreen","green","lightgreen"))(100)
+rgBk<- colorRampPalette(c("red","green","black"))(100)
+
+#plot the images and assign the color
+
+par(mfrow=c(2,3))
+plot(Aug_08_2014, col=clb)
+plot(Aug_08_2014, col=clg)
+plot(Aug_01_2021, col=clb)
+plot(Aug_01_2021, col=clg)
+plot(Aug_21_2021, col=clb)
+plot(Aug_21_2021, col=clg)
+
+#i want to see the images from january 2014 and january 2020
+#extraxt them from the stack
+
+
+#I want to see if there is a difference in this two period
+difcl<-colorRampPalette(c("yellow","red","black"))(100)#create the color ramp palette
+Vegdif<-Aug_08_2014-Aug_21__2021 #make the difference
+plot(Vegdif, col=difcl)#plotting the images
+
+plot(Vegdif, col = rev(terrain.colors(10)), main = "layer")
+
+
+ plot(Agosto2014_un$map)
+ plot(Agosto2014_un$map, col=clb)
+ Agosto2014_un <- unsuperClass(august2014, nClasses=2)
+ plot(Agosto2014_un$map, col=clb)
+ Agosto2014_un <- unsuperClass(august2014, nClasses=4)
+ plot(Agosto2014_un$map, col=clb)
+ Agosto2014_un <- unsuperClass(august2014, nClasses=5)
+ plot(Agosto2014_un$map, col=clb)
+ Agosto2014_un <- unsuperClass(august2014, nClasses=8)
+ plot(Agosto2014_un$map, col=clb
 
 cveg <- crop(a, ext)
 cveg1 <- crop(b, ext)
@@ -140,7 +296,159 @@ napa
 # values     : 1, 2  (min, max)
 plot(napa$map)
 
+ext <- c(15.5, 16.5, 37.8, 38.5) # provincia di Reggio, Asprumunti.
 
+
+######################### make the frequencies of the precences of 2014 ############################
+
+v2014 <- raster("c_gls_NDVI300_201408010000_GLOBE_PROBAV_V1.0.1.nc")
+v2014
+#class      : RasterLayer 
+#dimensions : 47040, 120960, 5689958400  (nrow, ncol, ncell)
+#resolution : 0.00297619, 0.00297619  (x, y)
+#extent     : -180.0015, 179.9985, -59.99851, 80.00149  (xmin, xmax, ymin, ymax)
+#crs        : +proj=longlat +datum=WGS84 +no_defs 
+#source     : c_gls_NDVI300_201408010000_GLOBE_PROBAV_V1.0.1.nc 
+#names      : Normalized.Difference.Vegetation.Index.333M 
+#zvar       : NDVI 
+# crop the place of interest, cut the large extent
+ext <- c(15.6, 16.9436, 37.8463, 38.4) # provincia di Reggio, Asprumunti.
+v2014c <- crop(v2014, ext)
+plot(v2014c)
+
+      
+      
+      
+      
+      
+      
+      ############################# different cropping ###############################
+# frequencies unsuperClass
+v2014c_un <- unsuperClass(v2014c, nClasses=2)
+plot(v2014c)
+plot(v2014c_un)
+v2014c_un
+> v2021c_un <- unsuperClass(crop_Aug_21_2021, nClasses=2)
+> 
+> plot(v2021c_un)
+Errore in xy.coords(x, y, xlabel, ylabel, log) : 
+  'x' is a list, but does not have components 'x' and 'y'
+> v2021c_un
+unsuperClass results
+
+*************** Map ******************
+$map
+class      : RasterLayer 
+dimensions : 114, 186, 21204  (nrow, ncol, ncell)
+resolution : 0.00297619, 0.00297619  (x, y)
+extent     : 15.64435, 16.19792, 37.94494, 38.28423  (xmin, xmax, ymin, ymax)
+crs        : +proj=longlat +datum=WGS84 +no_defs 
+source     : memory
+names      : layer 
+values     : 1, 2  (min, max)
+
+> plot(v2021c_un$map)
+> v2021c_un <- unsuperClass(crop_Aug_21_2021, nClasses=3)
+> plot(v2021c_un$map)
+> frequencies <-v2021c_un$map
+> freq(frequencies)
+     value count
+[1,]     1  6052
+[2,]     2  6103
+[3,]     3  9049
+> 6052+ 6103
+[1] 12155
+> 6052+ 6103+ 9049
+[1] 21204
+> tot <- 21204
+> freq_vegetation_2021 <- 12155/tot
+> freq_vegetation_2021 
+[1] 0.5732409
+# unsuperClass results
+
+# *************** Map ******************
+# $map
+# class      : RasterLayer 
+# dimensions : 235, 336, 78960  (nrow, ncol, ncell)
+# resolution : 0.00297619, 0.00297619  (x, y)
+# extent     : 15.49851, 16.49851, 37.79911, 38.49851  (xmin, xmax, ymin, ymax)
+# crs        : +proj=longlat +datum=WGS84 +no_defs 
+# source     : memory
+# names      : layer 
+# values     : 1, 2  (min, max)
+
+plot(v2014c_un$map)
+freq(v2014c_un$map)
+#      value count
+# [1,]     1 58950
+# [2,]     2 20010
+58950 + 20010
+# [1] 78960
+
+propveg2014_1 <- 58950/totale 
+propveg2014_2 <- 20010/totale
+ 
+propveg2014_1 
+# [1] 0.7465805
+propveg2014_2 
+# [1] 0.2534195
+
+cover <- c("forest", "deforestation")
+prop2014 <- c(propforest, propagri)
+proportion <- data.frame(cover, prop2014)
+p1 <- ggplot(proportion, aes(x=cover, y= prop2014, color=cover)) + geom_bar(stat="identity", fill="white")  + ylim(0,1)
+
+
+
+
+
+
+
+
+
+
+plot(l2017c)
+Errore in h(simpleError(msg, call)) : 
+  errore durante la valutazione dell'argomento 'x' nella selezione di un metodo per la funzione 'plot': oggetto 'l2017c' non trovato
+ plot(l2107c$map)
+ 
+ plot(v2014c)
+ v2014c_un <- unsuperClass(v2014, nClasses=2)
+ 
+ v2014c_un <- unsuperClass(v2014c, nClasses=2)
+ plot(v2014c)
+ plot(v2014c_un)
+Errore in xy.coords(x, y, xlabel, ylabel, log) : 
+  'x' is a list, but does not have components 'x' and 'y'
+v2014c_un
+unsuperClass results
+
+*************** Map ******************
+$map
+class      : RasterLayer 
+dimensions : 235, 336, 78960  (nrow, ncol, ncell)
+resolution : 0.00297619, 0.00297619  (x, y)
+extent     : 15.49851, 16.49851, 37.79911, 38.49851  (xmin, xmax, ymin, ymax)
+crs        : +proj=longlat +datum=WGS84 +no_defs 
+source     : memory
+names      : layer 
+values     : 1, 2  (min, max)
+
+> plot(v2014c_un$map)
+> freq(v2014c_un$map)
+     value count
+[1,]     1 58950
+[2,]     2 20010
+> 58950 + 20010
+[1] 78960
+> 
+> propveg2014_1 <-58950/totale
+> propveg2014_2 <-20010/totale
+> 
+> propveg2014_1 
+[1] 0.7465805
+> propveg2014_2 
+[1] 0.2534195
 
 #####
 
@@ -240,7 +548,7 @@ null device
 > plot(veg_index)
 > plot(veg_index$[[1]])
 Errore: '[[' inatteso in "plot(veg_index$[["
-> plot(veg_index[[1]])
+plot(veg_index[[1]])
 > hist(veg_index)
 Messaggi di avvertimento:
 1: In .hist1(raster(x, y[i]), maxpixels = maxpixels, main = main[y[i]],  :
